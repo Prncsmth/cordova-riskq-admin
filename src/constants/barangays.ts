435b3ed37a85a19169c3e1faba3dc001f3547411
carl <kianrapada4@gmail.com>
2026-09-04 15:41:28 +0800

    feat: add Cordova barangay names constant

---
+++ src/constants/barangays.ts
@@ -0,0 +1,17 @@
+// Mirrors the barangay names in the mobile repo's constants/cordovaBarangays.ts.
+// Kept in sync manually -- no shared package between the two repos today.
+export const CORDOVA_BARANGAY_NAMES = [
+  "Alegria",
+  "Bangbang",
+  "Buagsong",
+  "Catarman",
+  "Cogon",
+  "Dapitan",
+  "Day-as",
+  "Gabi",
+  "Gilutongan",
+  "Ibabao",
+  "Pilipog",
+  "Poblacion",
+  "San Miguel",
+];
