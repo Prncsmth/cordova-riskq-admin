563043b39e8c326305d0454ecc294ad2e432bfb1
kian <kianrapada@gmail.com>
2026-09-27 01:59:57 +0800

    debuging socket io

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -6,7 +6,7 @@ export const API_URL =
 
 export const SOCKET_URL =
   process.env.NEXT_PUBLIC_SOCKET_URL ||
-  "http://localhost:5000";
+  "http://localhost:8000";
 
 export const EMERGENCY_STATUSES = [
   "Active",
